2760ffeb4305ba8f3070259e04f275f5510d85b4
BrunoC21 <bruno.caro2101@alumnos.ubiobio.cl>
2025-05-24 18:53:12 -0400

    Cambios para crear bodega

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GestionInventarioComponent } from "./pages/gestion-inventario/gestion-inventario.component";
 import { GestionProductoComponent } from './pages/gestion-producto/gestion-producto.component';
+import { GestionBodegaComponent } from './pages/gestion-bodega/gestion-bodega.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, GestionInventarioComponent, GestionProductoComponent],
+  imports: [RouterOutlet, GestionInventarioComponent, GestionProductoComponent, GestionBodegaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/gestion-bodega/gestion-bodega.component.ts
+++ src/app/pages/gestion-bodega/gestion-bodega.component.ts
@@ -4,7 +4,7 @@ import { FilterPipe } from '../../pipes/filter.pipe';
 import { LimitPipe } from '../../pipes/limit-pipe.pipe';
 import { PaginatePipe } from '../../pipes/paginate.pipe';
 import { FormsModule } from '@angular/forms';
-import { SucursalService } from '../../services/sucursal.service';
+import { SucursalService, Warehouse } from '../../services/sucursal.service';
 
 @Component({
   selector: 'app-gestion-bodega',
@@ -13,8 +13,29 @@ import { SucursalService } from '../../services/sucursal.service';
   templateUrl: './gestion-bodega.component.html',
   styleUrls: ['./gestion-bodega.component.css']
 })
-export class GestionBodegaComponent {
+export class GestionBodegaComponent implements OnInit{
   currentPage = 1;
+  selectedLimit = 5;
+  filterPost = "";
+
+  posts: Warehouse[] = [];
+
+  newWarehouse: Warehouse = {
+    warehouse_Name: '',
+    direction: ''
+  };
+
+  constructor(private sucursalService: SucursalService) {}
+
+  ngOnInit(): void {
+    this.loadWarehouses();
+  }
+
+  loadWarehouses(): void {
+    this.sucursalService.getAll().subscribe(data => {
+      this.posts = data;
+    });
+  }
 
   get totalPages(): number {
     return Math.ceil((this.posts.filter(post =>
@@ -22,23 +43,34 @@ export class GestionBodegaComponent {
     ).length || 1) / this.selectedLimit);
   }
 
-  selectedLimit = 5; // valor por defecto
   get filteredPosts() {
     const filtered = this.posts.filter(post =>
       post.warehouse_Name.toLowerCase().includes(this.filterPost.toLowerCase())
     );
-    return filtered.slice(0, this.selectedLimit);
+    return filtered.slice(
+      (this.currentPage - 1) * this.selectedLimit,
+      this.currentPage * this.selectedLimit
+    );
   }
   
-  constructor(){}
-  filterPost = "";
-  posts = [
-    {
-      "id": 1,
-      "warehouse_Name": "Sucursal los vilos",
-      "direccion": "123 calle pan"
+  addWarehouse(): void {
+    const { warehouse_Name, direction } = this.newWarehouse;
+
+    if (warehouse_Name.trim() && direction.trim()) {
+      this.sucursalService.create(this.newWarehouse).subscribe(() => {
+        this.loadWarehouses(); // recargar productos
+        this.newWarehouse = {
+          warehouse_Name: '',
+          direction: ''
+        };
+
+        const popup = document.getElementById('popup-agregar') as HTMLElement;
+        if (popup) popup.removeAttribute('popover');
+      });
+    } else {
+      alert('Todos los campos son obligatorios.');
     }
-  ];
-  ngOnInit() {  
   }
+  
+  
 }
