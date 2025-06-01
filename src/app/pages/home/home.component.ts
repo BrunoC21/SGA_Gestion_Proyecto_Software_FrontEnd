c623b1ae49cf8b7a944efe17847daa4e3aa6b780
000Matt000 <rodrigo.jara2101@alumnos.ubiobio.cl>
2025-06-01 16:07:17 -0400

    Modificaciones home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,38 +1,39 @@
-import { AfterViewInit, Component, ElementRef, ViewChild, CommonModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+
+export interface Producto {
+  imagen: string;
+  titulo: string;
+  descripcion: string;
+  precio: number;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements AfterViewInit {
-  @ViewChild('manualCarouselTrack') trackRef!: ElementRef;
-
-  productos = [
-    { nombre: 'Paracetamol', precio: 1990, imagen: 'assets/images/med1.jpg' },
-    { nombre: 'Ibuprofeno', precio: 2590, imagen: 'assets/images/med2.jpg' },
-    { nombre: 'Aspirina', precio: 1890, imagen: 'assets/images/med3.jpg' },
-    { nombre: 'Amoxicilina', precio: 3290, imagen: 'assets/images/med4.jpg' },
-    { nombre: 'Omeprazol', precio: 2990, imagen: 'assets/images/med5.jpg' },
-    { nombre: 'Vitamina C', precio: 1590, imagen: 'assets/images/med6.jpg' },
-    { nombre: 'Loratadina', precio: 1990, imagen: 'assets/images/med7.jpg' },
-    { nombre: 'Antigripal', precio: 2390, imagen: 'assets/images/med8.jpg' }
+export class HomeComponent {
+  productos: Producto[] = [
+    {  imagen: 'assets/img/producto1.jpg',  titulo: 'Nombre del Producto1',  descripcion: 'Descripción breve del producto.',  precio: 8990},
+    {  imagen: 'assets/img/producto1.jpg',  titulo: 'Nombre del Producto2',  descripcion: 'Descripción breve del producto.',  precio: 4990},
+    {  imagen: 'assets/img/producto1.jpg',  titulo: 'Nombre del Producto3',  descripcion: 'Descripción breve del producto.',  precio: 5990},
+    {  imagen: 'assets/img/producto1.jpg',  titulo: 'Nombre del Producto4',  descripcion: 'Descripción breve del producto.',  precio: 6990},
+    {  imagen: 'assets/img/producto1.jpg',  titulo: 'Nombre del Producto5',  descripcion: 'Descripción breve del producto.',  precio: 8990},
+    {  imagen: 'assets/img/producto1.jpg',  titulo: 'Nombre del Producto6',  descripcion: 'Descripción breve del producto.',  precio: 9990}
+    
   ];
 
-  ngAfterViewInit() {
-    const track = this.trackRef.nativeElement as HTMLElement;
-    const btnNext = document.querySelector('.next-manual') as HTMLButtonElement | null;
-    const btnPrev = document.querySelector('.prev-manual') as HTMLButtonElement | null;
+  startIndex: number = 0;
 
-    const scrollAmount = track.clientWidth / 5 * 2.5; // desplaza 2.5 elementos
+  mostrarPrev(): void {
+    if (this.startIndex > 0) {
+      this.startIndex--;
+    }
+  }
 
-    btnNext?.addEventListener('click', () => {
-      track.scrollBy({ left: scrollAmount, behavior: 'smooth' });
-    });
-
-    btnPrev?.addEventListener('click', () => {
-      track.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
-    });
+  mostrarNext(): void {
+    if (this.startIndex < this.productos.length - 4) {
+      this.startIndex++;
+    }
   }
 }
