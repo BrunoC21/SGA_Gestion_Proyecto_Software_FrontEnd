4d69dd2390d202b344ba85afd8e42c4ec806921b
000Matt000 <rodrigo.jara2101@alumnos.ubiobio.cl>
2025-06-01 22:49:03 -0400

    beneficios
    crecion de beneficios.
    modificaciones en los estilos de gestion de bodegas.

--- src/app/pages/beneficios/beneficios.component.ts
+++ src/app/pages/beneficios/beneficios.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FilterPipe } from '../../pipes/filter.pipe';
+import { LimitPipe } from '../../pipes/limit-pipe.pipe';
+import { PaginatePipe } from '../../pipes/paginate.pipe';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-beneficios',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, FilterPipe, LimitPipe, PaginatePipe],
   templateUrl: './beneficios.component.html',
-  styleUrl: './beneficios.component.css'
+  styleUrls: ['./beneficios.component.css']
 })
-export class BeneficiosComponent {
 
+export class BeneficiosComponent {
+  
 }
